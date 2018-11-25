4bb2d8adb3b1c145213adfa01bb7aa92b56e998a
eramanav <30555282+eramanav@users.noreply.github.com>
2018-11-24 19:33:57 -0800

    Create get_jenkin_install.sh

---
+++ get_jenkin_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# install Docker
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt update
+
+apt-cache policy docker-ce
+
+sudo apt install docker-ce
+
+sude usermod -aG docker rprakash
+
+systemctl enable docker
+
+systemctl start docker
+
+# run jenkin
+
+mkdir -p /var/jenkins_home
+
+chown -R 1000:1000 /var/jenkins_home/
+
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins
+
+
+
